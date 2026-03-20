5dcbe4c87dd00b78c4ee2c17a8816c882b21dbf4
huntereye76 <huntereye76@gmail.com>
2026-03-20 10:12:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ from keywords import KEYWORDS
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 session_string = os.getenv("SESSION_STRING")
-# FIX: remove unwanted line breaks
-session_string = session_string.replace("\n", "").replace("\r", "").strip()
+# 🔥 SUPER CLEAN (handles all hidden chars)
+session_string = re.sub(r"\s+", "", session_string)
 DB_URL = os.getenv("DB_URL")
 
 print("Session length:", len(session_string))
